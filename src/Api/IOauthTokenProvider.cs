0426e6dd15c7350d9a3ccde45853f8a224509980
fatal-me <82995789+fatal-me@users.noreply.github.com>
2022-11-03 10:30:08 +0400

    Провайдер токенов OAuth 2.0. (#6)

---
+++ src/Api/IOauthTokenProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sungero.Plugins.Sdk
+{
+  /// <summary>
+  /// Интерфейс провайдера токенов OAuth 2.0.
+  /// </summary>
+  public interface IOauthTokenProvider
+  {
+    /// <summary>
+    /// Создать токен.
+    /// </summary>
+    /// <param name="pluginId">Идентификатор плагина.</param>
+    /// <returns>Токен OAuth 2.0.</returns>
+    string CreateToken(Guid pluginId);
+  }
+}
---
+++ src/Api/OauthTokens.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Sungero.Plugins.Sdk
+{
+  /// <summary>
+  /// Класс для получения токенов OAuth 2.0.
+  /// </summary>
+  public static class OauthTokens
+  {
+    /// <summary>
+    /// Провайдер токенов.
+    /// </summary>
+    private static Lazy<IOauthTokenProvider> internalTokenProvider;
+
+    /// <summary>
+    /// Установить провайдер.
+    /// </summary>
+    /// <param name="tokenProvider">Провайдер токенов.</param>
+    public static void SetTokenProvider(Lazy<IOauthTokenProvider> tokenProvider)
+    {
+      internalTokenProvider = tokenProvider;
+    }
+
+    /// <summary>
+    /// Создать токен.
+    /// </summary>
+    /// <param name="pluginId">Идентификатор плагина.</param>
+    /// <returns>Токен OAuth 2.0.</returns>
+    public static string CreateToken(Guid pluginId)
+    {
+      if (internalTokenProvider is null)
+      {
+        throw new ArgumentException("No token provider.");
+      }
+
+      return internalTokenProvider.Value.CreateToken(pluginId);
+    }
+  }
+}
