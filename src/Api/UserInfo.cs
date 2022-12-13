c08bbc4306a704f31c28a2323901eb6e5ba8e909
Полин Алексей (Polin_AY) <Polin_AY@directum.ru>
2022-12-13 16:33:55 +0400

    Merged PR 26063: [13.12] Добавил точку доступа UserInfo
    Добавил точку доступа UserInfo и интерфейс IUserInfo, который умеет возвращать текущую культуру пользователя
    
    Related work items: #218933

---
+++ src/Api/UserInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Sungero.Plugins.Sdk
+{
+  /// <summary>
+  /// Класс для получения информации о пользователе.
+  /// </summary>
+  public static class UserInfo
+  {
+    /// <summary>
+    /// Получатель информации о пользователе.
+    /// </summary>
+    private static Lazy<IUserInfoGetter> userInfoGetter;
+    
+    /// <summary>
+    /// Установить объект для получения информации о пользователе.
+    /// </summary>
+    /// <param name="userInfoGetter">Ленивый объект для получения информации о пользователе.</param>
+    public static void SetUserInfoGetter(Lazy<IUserInfoGetter> userInfoGetter)
+    {
+      UserInfo.userInfoGetter = userInfoGetter;
+    }
+
+    /// <summary>
+    /// Культура пользователя.
+    /// </summary>    
+    public static CultureInfo CultureInfo
+    {
+      get
+      {
+        CheckUserInfoGetter();
+
+        return userInfoGetter.Value.CultureInfo;
+      }
+    }
+    
+    /// <summary>
+    /// Проверить наличие объекта для получения информации о пользователе.
+    /// </summary>
+    private static void CheckUserInfoGetter()
+    {
+      if (userInfoGetter?.Value == null)
+        throw new NullReferenceException("UserInfoGetter has no implementation.");
+    }    
+  }
+}
