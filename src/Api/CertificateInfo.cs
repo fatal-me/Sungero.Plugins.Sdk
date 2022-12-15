61f9adefe47a270f39adfa642c7b3412e19447f3
Полин Алексей (Polin_AY) <Polin_AY@directum.ru>
2022-12-15 23:32:19 +0400

    Merged PR 26141: [15.12] Точка расширения CertificateInfo
    Добавил в SDK класс и интерфейс для получения информации о параметрах сертификата
    
    Related work items: #218932

---
+++ src/Api/CertificateInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sungero.Plugins.Sdk
+{
+  /// <summary>
+  /// Класс для получения информации о сертификате.
+  /// </summary>  
+  public static class CertificateInfo
+  {
+    /// <summary>
+    /// Получатель информации о сертификате.
+    /// </summary>
+    private static Lazy<ICertificateInfoGetter> certificateInfoGetter;
+    
+    /// <summary>
+    /// Установить объект для получения информации о сертификате.
+    /// </summary>
+    /// <param name="certificateInfoGetter">Ленивый объект для получения информации о сертификате.</param>
+    public static void SetCertificateInfoGetter(Lazy<ICertificateInfoGetter> certificateInfoGetter)
+    {
+      CertificateInfo.certificateInfoGetter = certificateInfoGetter;
+    }
+
+    /// <summary>
+    /// Получить параметры сертификата.
+    /// </summary>
+    /// <returns>Параметры.</returns>  
+    public static IDictionary<string, string> GetParameters()
+    {
+      CheckCertificateInfoGetter();
+
+      return certificateInfoGetter.Value.GetParameters();
+    }
+    
+    /// <summary>
+    /// Проверить наличие объекта для получения информации о сертификате.
+    /// </summary>
+    private static void CheckCertificateInfoGetter()
+    {
+      if (certificateInfoGetter?.Value == null)
+        throw new NullReferenceException("CertificateInfoGetter has no implementation.");
+    }        
+  }
+}
