ae1770a621b4786892fee980b36c03b8db5eea84
Барсуков Андрей (Barsukov_AL) <Barsukov_AL@directum.ru>
2023-04-28 17:59:39 +0400

    Merged PR 29146: SDK плагинов. Добавлен интерфейс ISimpleCryptographyPlugin
    Новый интерфейс плагина ISimpleCryptographyPlugin для подписания документов по телу, а не по хешу.
    
    (cherry picked from commit a4a7a4ba5ccd357844f53f687465cc53d72f6090)
    
    Related work items: #244296

---
+++ src/Cryptography/ISimpleCryptographyPlugin.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Sungero.Cryptography.Shared
+{
+  /// <summary>
+  /// Плагин упрощенного подписания.
+  /// </summary>
+  public interface ISimpleCryptographyPlugin : ICryptographyPlugin
+  {
+    /// <summary>
+    /// Подписать.
+    /// </summary>
+    /// <param name="dataProvider">Поставщик данных.</param>
+    /// <param name="certificate">Сертификат.</param>
+    /// <returns>Подпись.</returns>
+    byte[] Sign(ICryptographyPluginDataProvider dataProvider, X509Certificate2 certificate);
+
+    /// <summary>
+    /// Проверить подпись данных.
+    /// </summary>
+    /// <param name="dataProvider">Поставщик данных.</param>
+    /// <param name="signature">Подписанные данные.</param>
+    /// <returns>True, если подпись верна.</returns>
+    bool VerifySignature(ICryptographyPluginDataProvider dataProvider, byte[] signature);
+  }
+}
